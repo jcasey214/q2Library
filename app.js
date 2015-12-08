c8064fe8c4680b9692b2f42970b101b527f1a9a0
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-08 12:22:57 -0700

    Added fortune-cookie module to make cow say random fortunes

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var cowsay = require('cowsay');
+var fortune = require('fortune-cookie');
 
 var app = express();
 
 var books = ["Javascript-The Good Parts", "Eloquent Javascript",
 "Horton Hears a Who"];
 
-app.get('/cowsay/:cowsays', function(request, response){
+app.get('/cowsay', function(request, response){
   response.setHeader('content-type','text/html');
   response.send(
     "<pre>" +
-      cowsay.say({'text': request.params.cowsays}) +
+      cowsay.say({'text': fortune[Math.floor(Math.random() * 250)]}) +
     "</pre>"
   );
-})
+});
 
 app.get('/', function(request, response){
   response.json(books);
