fd1c2cbd8e724b23c6d053c19f9e439efb3c3655
Christopher Hutchinson <cshutchinson@gmail.com>
2015-12-08 11:28:58 -0700

    cowsay added to repeat text to browser

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var cowsay = require('cowsay');
+
 var app = express();
 
+var books = ["Javascript-The Good Parts", "Eloquent Javascript",
+"Horton Hears a Who"];
+
+app.get('/cowsay/:cowsays', function(request, response){
+  response.send(cowsay.say({'text': request.params.cowsays}));
+})
+
 app.get('/', function(request, response){
-  response.send('hello world');
+  response.json(books);
 });
 
 app.listen(8080);
