9aaff9a28b49f32d1cd8395f4e271791fe9bde7e
Jonathan Casey <jon_casey@me.com>
2015-12-09 08:42:21 -0700

    add fortune at /cowsay

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ app.get('/cowsay', function(request, response){
   );
 });
 
+app.get('/cowsay/:cowsays', function(request, response){
+  response.setHeader('content-type','text/html');
+  response.send(
+    "<pre>" +
+      cowsay.say({'text': request.params.cowsays}) +
+    "</pre>"
+  );
+});
+
 app.get('/', function(request, response){
   response.json(books);
 });
